9f0c94475b9606647e4ff93c89295d77741be5da
Vanne Vega <manelp3214@gmail.com>
2022-10-01 15:43:11 -0500

    terminamos promise Race

--- prom-3.js
+++ prom-3.js
@@ -1,8 +1,28 @@
-function sumarLento(nuemero) {
+function retornaTrue() {
+    return true;
+}
+
+function sumarLento(numero) {
     return new Promise(function(resolve, reject) {
         setTimeout(function() {
             resolve (numero + 1);
+            //reject ('Sumar lento fallo');
 
         }, 800 );
     });
 }
+let sumarRapido = (numero) => {
+    return new Promise( (resolve, reject) => {
+        setTimeout( () => resolve( numero + 1 ), 300 );
+    });
+}
+let cosas = [sumarRapido(10), sumarLento(5), true, 'Hola mundo', retornaTrue()];
+
+Promise.all(cosas)
+     .then(repuestas => {
+         console.log(repuestas);
+     })
+     .catch(console.log);
+
+//sumarRapido( 10 ).then( console.log );
+//sumarLento( 5 ).then( console.log );
